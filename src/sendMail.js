f4b3c13a31790d2cc8f62d3580e9da4472c07f09
dany han <dany@mdlabs.com>
2018-05-07 23:17:07 -0500

    Update email key

--- src/sendMail.js
+++ src/sendMail.js
@@ -1,23 +1,26 @@
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY || "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");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(
+  process.env.SENDGRID_API_KEY ||
+    "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"
+);
 
 module.exports = ({ to, from, subject, html }) => {
-
   const msg = {
-    to: to || 'danyhan89@gmail.com',
-    from: from || 'test@example.com',
-    subject: subject || 'Sending with SendGrid is Fun',
+    to: to || "danyhan89@gmail.com",
+    from: from || "successnailsalon2017@gmail.com",
+    subject: subject || "Sending with SendGrid is Fun",
 
-    html: html || '<strong>and easy to do anywhere, even with Node.js</strong>',
+    html: html || "<strong>and easy to do anywhere, even with Node.js</strong>"
   };
 
   const result = sgMail.send(msg);
-  result.then(response => {
-    
-    console.log('!!!!')
-    console.log(response)
-  }).catch(err => {
-    console.log(err)
-  })
-  console.log(result)
-}
+  result
+    .then(response => {
+      console.log("!!!!");
+      console.log(response);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  console.log(result);
+};
